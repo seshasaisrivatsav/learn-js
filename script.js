770a051b026f2b6c5c312b1221dcdaf972dcb198
seshasaisrivatsav <seshasaisrivatsav@gmail.com>
2018-02-18 13:21:14 -0500

    first assignment

---
+++ script.js
@@ -0,0 +1,18 @@
+let checkPrime = function (nr) {
+    if(typeof nr !== "number"){
+        console.log(nr + " is not of right format. Exiting..");
+        return;
+    }
+    if (nr === 2) console.log(nr + " is a prime number");
+    let isPrime = true;
+    for(let i=2; i<nr; i++) {
+        if(nr%i === 0) {
+            isPrime = false;
+            console.log(nr + " is not a prime number");
+            return;
+        }
+    }
+    console.log(nr + " is a prime number");
+    
+}
+ 
